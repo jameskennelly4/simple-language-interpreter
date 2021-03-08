70b8c15f431a96fd4a11f54abf15cab94dc4f437
wbm4563 <wbm4563@gmail.com>
2021-03-07 21:54:40 -0500

    more read-parse-tree updates
    finished params for various parse statements

--- M-integer.rkt
+++ M-integer.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require "simpleParser.rkt")
+
 (define M-eval
   (lambda (expression)
     (cond
@@ -34,7 +36,25 @@
   (lambda (expression)
     (caddr expression)))
 
+
+(define read-parse-tree
+  (lambda (parse-tree)
+    (cond
+      ((null? parse-tree) 'end)
+      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'var) (eq? (length (car parse-tree) 3))) (var-define-parse (cadar parse-tree) (cddar parse-tree)) (read-parse-tree (cdr parse-tree)))
+      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'var) (eq? (length (car parse-tree) 2))) (var-initialize-parse (cadar parse-tree)) (read-parse-tree (cdr parse-tree)))
+      ((and (list? (car parse-tree)) (eq? (caar parse-tree) '=)) (assign-parse (cadar parse-tree) (cddar parse-tree)) (read-parse-tree (cdr parse-tree)))
+      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'return)) (return-parse (cadar parse-tree)) (read-parse-tree (cdr parse-tree)))
+      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'if) (eq? (length (car parse-tree) 4))) (if-else-parse (cadar parse-tree) (caddar parse-tree) (cdddar parse-tree)) (read-parse-tree (cdr parse-tree)))
+      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'if) (eq? (length (car parse-tree) 3))) (if-parse (cadar parse-tree) (cddar parse-tree)) (read-parse-tree (cdr parse-tree)))
+      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'while)) (while-parse (cadar parse-tree) (cddar parse-tree) (read-parse-tree (cdr parse-tree))))
+      (else (read-parse-tree (cdr parse-tree))))))
+
 (define return-parse
+  (lambda (expression)
+    (return-parse-cleanup (M-eval expression))))
+
+(define return-parse-cleanup
   (lambda (output)
     (cond
       ((integer? output) output)
@@ -43,17 +63,31 @@
       ((eq? output #t) 'true)
       (else (error 'bad-output)))))
 
-(define read-parse-tree
-  (lambda (parse-tree)
-    (cond
-      ((null? parse-tree) 'end)
-      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'var)) (var-parse) (read-parse-tree (cdr parse-tree)))
-      ((and (list? (car parse-tree)) (eq? (caar parse-tree) '=)) (assign-parse (cadar parse-tree) (cddar parse-tree)) (read-parse-tree (cdr parse-tree)))
-      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'return)) (return-parse (cadar parse-tree)) (read-parse-tree (cdr parse-tree)))
-      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'if)) (if-parse) (read-parse-tree (cdr parse-tree)))
-      ((and (list? (car parse-tree)) (eq? (caar parse-tree) 'while)) (while-parse (cadar parse-tree) (cddar parse-tree) (read-parse-tree (cdr parse-tree))))
-      (else (read-parse-tree (cdr parse-tree))))))
+(define assign-parse
+  (lambda (var expression)
+    (add-binding-pair pairs var (M-eval expression))))
 
-(define print-output
-  (lambda (out)
-    (display out) (newline)))
+(define var-define-parse
+  (lambda (var value)
+    (add-binding-pair pairs var val)))
+
+(define var-initialize-parse
+  (lambda (var)
+    (add-binding-pair-var-only pairs var)))
+
+(define if-parse
+  (lambda (conditional body)
+    (if (M-eval conditional)
+        (read-parse-tree body)
+        #f)))
+
+(define if-else-parse
+  (lambda (condtional body else-statement)
+    (if (M-eval conditional)
+        (read-parse-tree body)
+        (read-parse-tree else-statement))))
+
+(define while-parse
+  (lambda (condtional body)
+    (if (M-eval conditional)
+        ((read-parse-tree body) (while-parse conditional body)))))
